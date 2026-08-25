bc140894e6378c8cdc79036530014f959164886b
jevenshirrell <jshirr790@west-mec.org>
2026-08-25 14:41:44 -0700

    Server setup
    pages + routing module

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const PORT = 3000
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+// routes
+const route = require('./routes')(app)
+
+route('/', 'index')
+route(/.*/, 'error')
+
+app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`)
+})
---
+++ routes.js
@@ -0,0 +1,10 @@
+const path = require('path')
+
+// factory function - called with the app preloaded
+module.exports = app => {
+    return (url, file) => {
+        app.get(url, (req,res) => {
+            res.sendFile(path.join(__dirname, 'views', file + '.html'))
+        })
+    }
+}
